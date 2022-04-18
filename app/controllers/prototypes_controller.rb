f00ee816e4de2da4d3f6655057165cc47a02659d
malvart <102654106+malvart@users.noreply.github.com>
2022-04-18 15:23:04 +0900

    投稿者以外のユーザーが、投稿者専用のページに遷移不可に

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :set_prototype, only: [:show, :edit, :update]
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -49,6 +50,12 @@ class PrototypesController < ApplicationController
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @prototype.user
+      redirect_to action: :index
+    end
   end
 
 end
